fde5180cbb37dd058ca08858e8dd5f9658ca1c81
elyas-hanafi <eliinsta79@gmail.com>
2024-11-29 10:27:06 +0330

    feat: update

--- app/manifest.ts
+++ app/manifest.ts
@@ -6,7 +6,7 @@ export default function manifest(): MetadataRoute.Manifest {
     short_name: "NextPWA",
     description: "A Progressive Web App built with Next.js",
     start_url: "/",
-    display: "standalone",
+    display: "fullscreen", // Ensures the app opens in full screen
     background_color: "#ffffff",
     theme_color: "#000000",
   };
