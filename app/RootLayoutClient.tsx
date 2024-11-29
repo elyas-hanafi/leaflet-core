4240dabe330b25170973fd4376c032463b996b9a
elyas-hanafi <eliinsta79@gmail.com>
2024-11-29 09:59:09 +0330

    feat: update

--- app/RootLayoutClient.tsx
+++ app/RootLayoutClient.tsx
@@ -19,30 +19,6 @@ export default function RootLayoutClient({ children }: any) {
         });
     }
   }, []);
-  React.useEffect(() => {
-    if ("Notification" in window && "serviceWorker" in navigator) {
-      // Request notification permission
-      Notification.requestPermission().then((permission) => {
-        if (permission === "granted") {
-          console.log("Notification permission granted.");
-
-          // Show notification every 5 seconds
-          setInterval(() => {
-            navigator.serviceWorker.getRegistration().then((registration) => {
-              if (registration) {
-                registration.showNotification("PWA Setup", {
-                  body: "This notification repeats every 5 seconds.",
-                  icon: "/path-to-your-icon/icon.png", // Optional icon
-                });
-              }
-            });
-          }, 10000); // 5000 milliseconds = 5 seconds
-        } else {
-          alert("Notification permission denied.");
-        }
-      });
-    }
-  }, []);
 
   return <div className="">{children}</div>;
 }
