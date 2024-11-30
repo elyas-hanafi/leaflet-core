5972e4e830ab667d88c08dc4927cbb8b78c744cc
elyas-hanafi <eliinsta79@gmail.com>
2024-12-01 01:05:19 +0330

    feat: update notif

--- app/RootLayoutClient.tsx
+++ app/RootLayoutClient.tsx
@@ -4,17 +4,17 @@
 import React, { useEffect } from "react";
 
 // Helper function to convert VAPID public key
-function urlBase64ToUint8Array(base64String: string): Uint8Array {
-  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
-  const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
-  const rawData = atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
+// function urlBase64ToUint8Array(base64String: string): Uint8Array {
+//   const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+//   const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
+//   const rawData = atob(base64);
+//   const outputArray = new Uint8Array(rawData.length);
 
-  for (let i = 0; i < rawData.length; i++) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
-}
+//   for (let i = 0; i < rawData.length; i++) {
+//     outputArray[i] = rawData.charCodeAt(i);
+//   }
+//   return outputArray;
+// }
 
 export default function RootLayoutClient({ children }: any) {
   useEffect(() => {
